6df2d16cb12886460ef9225360c60a369831dad9
Ilinicz <vilinicz@mail.ru>
2013-04-10 05:03:45 +0400

    logout bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user])
   	if @user.save
-  		flash[:success] = "Welcome to the Sample App!"
+      sign_in @user
+  		flash[:success] = "Welcome to the Twirr!"
   		redirect_to @user
   	else
   		render 'new'
