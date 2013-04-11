98bd35da0b38de76f739af82c86cf6e6f80ba5a9
Ilinicz <vilinicz@mail.ru>
2013-04-12 03:34:37 +0400

    admin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,7 +56,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
-        
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 
 end
