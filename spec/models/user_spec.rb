5b80607599048b935538bf7b5c1e526dcd6eec01
Ilinicz <vilinicz@mail.ru>
2013-05-30 06:43:23 +0400

    Flow

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -151,10 +151,20 @@ describe User do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+      let(:followed_user) {FactoryGirl.create(:user)}
+
+      before do
+        @user.follow!(followed_user)
+        3.times {followed_user.microposts.create!(content:"Lorem Ipsum")}
+      end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+      end
     end
   end
 
@@ -179,4 +189,5 @@ describe User do
       its(:followed_users) { should_not include(other_user) }
     end
   end
+end
 end
