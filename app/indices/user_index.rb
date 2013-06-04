3adf50e90537b88690160569846a91d3cf11c189
Vincent Ilinicz <vilinicz@mail.ru>
2013-06-04 06:52:17 +0400

    No Sphinx

--- app/indices/user_index.rb
+++
@@ -1,7 +0,0 @@
-ThinkingSphinx::Index.define :user, :with => :active_record do
-  indexes subject, :sortable => true
-  indexes content
-  indexes user(:name), :as => :user, :sortable => true
-
-  has user_id, created_at, updated_at
-end
